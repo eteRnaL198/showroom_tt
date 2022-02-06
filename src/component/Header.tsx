d7651d737977e9a57d67c9b787346ee678fd6cb2
eteRnaL198 <ikuya313198@gmail.com>
2022-02-06 17:34:17 +0900

    first commit

---
+++ src/component/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <header
+        className="bg-pink-200 flex font-bold justify-center p-5 text-xl"
+      >
+        SHOWROOM Time Table
+      </header>
+    </>
+  )
+}
+
+export default Header;
