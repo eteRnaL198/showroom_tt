6b4763ac734758065912934ef7cb02d5b11431c8
eteRnaL198 <ikuya313198@gmail.com>
2022-03-03 21:55:52 +0900

    通知

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import * as functions from "firebase-functions";
 import LineClient from "./utility/line-client";
+import TimeUtil from "./utility/time-util";
 
 export const sendMessageToLine = functions.https.onRequest((request, response) => {
   const line = new LineClient(
@@ -13,3 +14,17 @@ export const sendMessageToLine = functions.https.onRequest((request, response) =
   response.send("send message to line");
 });
 
+// export const setScheduledNotification = functions.pubsub.schedule('').timeZone('Asia/Tokyo').onRun(() => {
+//   const line = new LineClient(
+//     "shs3qPU5K2gu76tWnmv7yy6cDNv3h4g5FN+B9gfFkSVdUO3I7I1NMTyxAwtZwLKrVlwM4TClbpC15JtK91ntNRotreMN19NAf/fov9ZfTQuLisuunYIQbsd6f7Eeifl1lbas7uRdPn6BK7gdTEYVegdB04t89/1O/w1cDnyilFU=",
+//     "c9a9906403973bc53e5742374a43f509"
+//   )
+//   line.send("星集めの時間です！")
+// })
+
+const setScheduledFunction = (schedule: string) => {
+  const cron = TimeUtil.toCron(schedule);
+  console.log(cron);
+}
+
+setScheduledFunction('2022-03-04-10:30');
---
+++ functions/src/utility/time-util.ts
@@ -0,0 +1,5 @@
+export default class TimeUtil {
+  static toCron (schedule: string) { // 2022-03-04-10:30 → 0 30 10 04 03 ? 2022
+
+  }
+}
